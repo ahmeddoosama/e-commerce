4a3fa214c44c998f5d0580f36c7d22174c27fa72
Ahmed Osama <ahmedosamabedawy@gmail.com>
2021-01-12 04:21:02 +0200

    Add resizing function

--- assets/js/vendors/10-slider-plugin.js
+++ assets/js/vendors/10-slider-plugin.js
@@ -22,18 +22,25 @@
             nextBtn: $this.find('.next'),
             prevBtn: $this.find('.prev'),
             move: 0,
-            clicked: false, 
+            clicked: false,
+            windowResizing: false
         }
 
         // Calc Slider Width item to show
-        slideObj.sliderItem.width(parseInt(slideObj.sliderBanner.width() / options.slidesToShow))
+        function resizing(){
 
-        slideObj.sliderBanner.width(slideObj.itemsLength * slideObj.sliderItem.outerWidth())
+            slideObj.sliderItem.width( parseInt( $this.width() / options.slidesToShow ) )
+            slideObj.sliderBanner.width(slideObj.itemsLength * slideObj.sliderItem.outerWidth())
+            slideObj.sliderBanner.css('transform', 'translateX(' + -(slideObj.move * slideObj.sliderItem.width()) + 'px)')
+
+            // console.log('resize')
+        }resizing();
+
 
         // Next Button Slider Function
         slideObj.nextBtn.click(function () {
 
-            if(!slideObj.clicked) {
+            if(!slideObj.clicked) { // > check user click on nextBtn
                 slideObj.clicked = true
                 slideObj.move = slideObj.move == (slideObj.itemsLength - 1) ? slideObj.itemsLength - 1 : slideObj.move + 1
                 slideObj.sliderBanner.css('transform', 'translateX(' + -(slideObj.move * slideObj.sliderItem.width()) + 'px)')
@@ -45,13 +52,21 @@
         // Previous Button Slider Function
         slideObj.prevBtn.click(function () {
 
-            if(!slideObj.clicked) {
+            if(!slideObj.clicked) { // > check user click on prevBtn
                 slideObj.clicked = true
                 slideObj.move = slideObj.move <= 0 ? 0 : slideObj.move - 1
                 slideObj.sliderBanner.css('transform', 'translateX(' + -(slideObj.move * slideObj.sliderItem.width()) + 'px)')
                 setTimeout(function(){ slideObj.clicked = false }, 505)
             }
 
+        })
+
+        $(window).on('resize', function(){
+            if(!slideObj.windowResizing) {
+                slideObj.windowResizing = true
+                resizing()
+                setTimeout(function(){slideObj.windowResizing = false}, 100)
+            }
         })
 
         // return
