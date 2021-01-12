bca5ae694f7242cd24eeac310058677218fa41d2
Ahmed Osama <ahmedosamabedawy@gmail.com>
2021-01-12 02:40:01 +0200

    Add (prevBtn and nextBtn) slider function

--- assets/js/vendors/10-slider-plugin.js
+++ assets/js/vendors/10-slider-plugin.js
@@ -15,11 +15,28 @@
             sliderBanner: $this.find('.slider__banner'),
             sliderItem: $this.find('.slider__item'),
             itemsLength: $this.find('.slider__item').length,
+            nextBtn: $this.find('.next'),
+            prevBtn: $this.find('.prev'),
+            move: 0
         }
 
         slideObj.sliderItem.width( parseInt( slideObj.sliderBanner.width() / options.slidesToShow ) );
 
         slideObj.sliderBanner.width( slideObj.itemsLength * slideObj.sliderItem.outerWidth() )
+
+
+        slideObj.nextBtn.click(function(){
+            slideObj.move = slideObj.move == (slideObj.itemsLength - 1) ? slideObj.itemsLength - 1 : slideObj.move + 1
+            slideObj.sliderBanner.css('transform', 'translateX(' + -(slideObj.move * slideObj.sliderItem.width()) + 'px)')
+        })
+
+        slideObj.prevBtn.click(function(){
+            slideObj.move = slideObj.move <= 0 ? 0 : slideObj.move - 1
+            slideObj.sliderBanner.css('transform', 'translateX(' + -(slideObj.move * slideObj.sliderItem.width()) + 'px)')
+        })
+
+
+
 
         return this
     }
